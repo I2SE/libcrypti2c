8b63de37a41495c09e39941523e1dbdeb7166f12
Stefan Wahren <stefan.wahren@i2se.com>
2018-04-24 16:41:22 +0200

    Revert "Fix cipher generation"
    This reverts commit bba21a6f80f45ce1e3ab81d89ae611ee3930d0fc.

--- src/atecc108_command.c
+++ src/atecc108_command.c
@@ -267,9 +267,14 @@ lca_priv_write_cmd (const int fd,
       tempkey = calc_digest(write_key, DATA_ZONE, write_key_slot, tempkey);
 
       // calc cipher text
-      for (i = 0; i < priv_key.len; i++)
+      for (i = 0; i < 4; i++)
         {
-          data.ptr[i] = priv_key.ptr[i] ^ tempkey.ptr[i];
+          data.ptr[i] = 0x00 ^ tempkey.ptr[i];
+        }
+
+      for (i = 0; i < priv_key.len - 4; i++)
+        {
+          data.ptr[i+4] = priv_key.ptr[i] ^ tempkey.ptr[i+4];
         }
 
       session_key = lca_sha256_buffer (tempkey);
